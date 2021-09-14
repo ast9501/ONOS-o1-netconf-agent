d8123b999478fed622b8bf922a938ffd1f0f3937
alan <ast9510.cs10@nycu.edu.tw>
2021-09-14 16:37:07 +0800

    Fix: add Error exception to handle null value from DMaaP

--- onosAgent/register/views.py
+++ onosAgent/register/views.py
@@ -16,15 +16,18 @@ def GetServerStatus(request):
 # start up registration to DMaaP topic
 def PnfRegToDmp(request):
     getMsg = 1
-    url = 'http://192.168.0.127:3904/events/unauthenticated.VES_PNFREG_OUTPUT/users/sdn-r?timeout=20000&limit=1000'
-    #while getMsg:
-    #    try:
-    data = requests.get(url)
-    d = data.json()
-    j = json.loads(d[0])
-    userName = j["event"]["pnfRegistrationFields"]["additionalFields"]["username"]
-    port = j["event"]["pnfRegistrationFields"]["additionalFields"]["oamPort"]
-    serverIp = j["event"]["pnfRegistrationFields"]["oamV4IpAddress"]
+    url = 'http://192.168.0.127:3904/events/unauthenticated.VES_PNFREG_OUTPUT/users/sdn-r?timeout=20000&limit=1'
+    while getMsg:
+        try:
+            data = requests.get(url)
+            d = data.json()
+            j = json.loads(d[0])
+            getMsg = 0
+        except (IndexError,KeyError):
+            getMsg = 1
+    userName = j['event']['pnfRegistrationFields']['additionalFields']['username']
+    port = j['event']['pnfRegistrationFields']['additionalFields']['oamPort']
+    serverIp = j['event']['pnfRegistrationFields']['oamV4IpAddress']
     #        getMsg = 0
     #    except:
     #        getMsg = 1
